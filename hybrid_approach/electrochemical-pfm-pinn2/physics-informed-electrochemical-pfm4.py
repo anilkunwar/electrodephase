256cf273a538e502af83eebda86dc8fd48c5dd70
Anil Kunwar <anilkunwar318@yahoo.com>
2026-01-03 11:44:51 +0100

    Update physics-informed-electrochemical-pfm4.py

--- hybrid_approach/electrochemical-pfm-pinn2/physics-informed-electrochemical-pfm4.py
+++ hybrid_approach/electrochemical-pfm-pinn2/physics-informed-electrochemical-pfm4.py
@@ -2747,8 +2747,10 @@ def plot_technique_specific_artifacts(obs_data: Dict) -> go.Figure:
             header=dict(values=['Parameter', 'Value'],
                        fill_color='paleturquoise',
                        align='left'),
-            cells=dict(values=[row[0] for row in table_data], 
-                      [row[1] for row in table_data],
+            cells=dict(values=[
+                           [row[0] for row in table_data], 
+                           [row[1] for row in table_data]
+                        ],
                       fill_color='lavender',
                       align='left'),
             columnwidth=[0.3, 0.7]
